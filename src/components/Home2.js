334cf8f65487f990cf434c4d339fc3876afd242f
mehmet <turmehmetkorkmaz@gmail.com>
2021-05-06 11:32:57 +0200

    react-tutorials-06/05

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -1,23 +1,50 @@
-import Card from "./Card"
-import "../pages/Home2.css"
-import data from "../pages/data.json"
+import Card from "./Card";
+import "../pages/Home2.css";
+import data from "../pages/data.json";
+import {useState} from "react"
 
-function Home2 () {
 
-  return(
-    <div>
+function Home2() {
 
-      <div className= "productList">
-      {
-        data.map(product=><Card product={product} key={product.id}/>)
-      }
+  const [searchedValue,setSearchedValue]=useState("")
+  const [filteredData,setfilteredData]=useState(data)
 
-      </div>
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(searchedValue);
+
+    const filteredProducts=data.filter(product=>{
       
+      const title=product.title.toUpperCase()
+      const text=searchedValue.toUpperCase()
+      return title.includes(text)  
+    
+    })
+    setfilteredData(filteredProducts);
+
+
+    }
+    const handleChange=(e)=>{
+      setSearchedValue(e.target.value);
+
+  }
+
+  return (
+    <div>
+      <div className="search">
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+          <button type="submit">search</button>
+        </form>
+      </div>
+
+      <div className="productList">
+        {data.map((product) => (
+          <Card product={product} key={product.id} />
+        ))}
+      </div>
     </div>
-  )
-
+  );
 }
-  
 
-export default Home2
+export default Home2;
